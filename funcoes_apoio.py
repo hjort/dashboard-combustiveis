b79ea605ab5000929ac0b2010f390bfafd7e462f
Rodrigo Hjort <rodrigo.hjort@serpro.gov.br>
2022-11-21 23:46:54 -0300

    Criados notebooks das perguntas 2 e 3

--- funcoes_apoio.py
+++ funcoes_apoio.py
@@ -9,7 +9,7 @@ def normalizar_bandeira(titulo):
         return 'OUTRAS'
 
 # função para retornar um dataframe preparado para uso
-def preparar_dataframe(qtd_linhas=5000):
+def preparar_dataframe(qtd_linhas=None, agrupar=False):
 
     # ler os dados do arquivo CSV
     df = pd.read_csv("ge-cv-2015-2020.csv.bz2",
@@ -22,19 +22,36 @@ def preparar_dataframe(qtd_linhas=5000):
     # limitar a quantidade de linhas
     if qtd_linhas:
         df = df.sample(n=qtd_linhas, random_state=42)
-    
+
     # criar coluna ano/mês da coleta
     df['ams'] = df['ano'].astype('int') * 100 + df['mes'].astype('int')
-    
+
     # criar coluna data da coleta
     df['data'] = pd.to_datetime(df['ams'], format='%Y%m')
 
-    # criar colunas lucro e %lucro
-    df['lucro'] = round(df['venda'] - df['compra'], 3)
-    df['plucro'] = round((df['venda'] / df['compra'] - 1) * 100, 2)
-
     # normalizar bandeira do posto
     df['bandeira'] = df['bandeira'].apply(normalizar_bandeira)
+    
+    # agrupar os dados pelas colunas
+    if agrupar:
+        #df = df.drop(['mes'], axis=1).\
+        df = df.\
+            groupby([
+                pd.Grouper(key='regiao'),
+                pd.Grouper(key='uf'),
+                pd.Grouper(key='produto'),
+                pd.Grouper(key='bandeira'),
+                pd.Grouper(key='ano'),
+                pd.Grouper(key='mes'),
+                pd.Grouper(key='ams'),
+            ]).mean().reset_index()
+
+    # criar colunas margem e %margem
+    df['margem'] = round(df['venda'] - df['compra'], 3)
+    df['pmargem'] = round((df['venda'] / df['compra'] - 1) * 100, 2)
+
+    # remover linhas que não possuam valor de venda
+    df.dropna(subset=['venda'], inplace=True)
 
     return df
 
